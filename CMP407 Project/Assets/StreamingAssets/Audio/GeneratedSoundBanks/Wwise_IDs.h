786d5f9757e41c34fe555b56bf1076e523775c1e
Cameron Thustain <1900594@uad.ac.uk>
2022-12-16 14:45:08 +0000

    working on switch footsteps

--- CMP407 Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CMP407 Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_STONE_FOOTSTEP = 2939822758U;
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
     } // namespace EVENTS
 
     namespace BANKS
