eedb4aeaeb6c1401e1fc26250c3700495649690d
Cameron Thustain <1900594@uad.ac.uk>
2022-12-19 12:59:17 +0000

    coming back to occlusion later

--- CMP407 Project/Assets/Scripts/OcclusionRaycast.cs
+++ CMP407 Project/Assets/Scripts/OcclusionRaycast.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class OcclusionRaycast : MonoBehaviour
 {
+
+    public GameObject AudioListener;
+
+    private float MaxDistOcclusion;
+
+    public bool OcclusionOn = false;
+
+    public string RTPC_Name = "RTPC_Occlusion_LoPass";
+    public string RTPC_Volume = "RTPC_Occlusion_Volume";
+
+
+    public float LoPassMax = 1;
+    public float VolumeMax = 1;
+    public string Listener = "Main Camera";
+    
+    
     // Start is called before the first frame update
     void Start()
     {
