d2d94d7a9a379d65419c6d4a7bb5d229df460a03
Cameron Thustain <1900594@uad.ac.uk>
2023-01-04 17:33:28 +0000

    made build

---
+++ Build/CMP407 Project_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUTTONCLICK = 4051332235U;
+        static const AkUniqueID PLAYFOOTSTEPS = 1088348632U;
+        static const AkUniqueID PLAYRADIO = 2507646724U;
+        static const AkUniqueID PLAYSTARSOUND = 1058272U;
+        static const AkUniqueID PLAYTORCHSOUND = 4132651872U;
+        static const AkUniqueID PLAYTREASURESOUND = 3703603361U;
+        static const AkUniqueID PLAYWALLCLOSE = 1889050313U;
+        static const AkUniqueID PLAYWALLOPEN = 30498459U;
+        static const AkUniqueID PLAYWILDLIFE_AMBIENCE = 121361976U;
+        static const AkUniqueID STOPSTARSOUND = 3543552222U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPTERRAIN
+        {
+            static const AkUniqueID GROUP = 548879402U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPTERRAIN
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID GAMESOUNDBANK = 460270446U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
